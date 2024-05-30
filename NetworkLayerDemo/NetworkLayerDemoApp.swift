a2e2afdf20ba8f261083170d88e6c234f6890a06
ahmed hussien <33827384+eng-ahmedhussien@users.noreply.github.com>
2024-05-30 16:09:56 +0300

    Initial Commit

---
+++ NetworkLayerDemo/NetworkLayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkLayerDemoApp.swift
+//  NetworkLayerDemo
+//
+//  Created by ahmed hussien on 30/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkLayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
